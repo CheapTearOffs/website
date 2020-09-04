9983b58d0e8bd52b67afe752d988af37f639c17b
Rey Vera <rvera@prosoft-technology.com>
2020-09-04 13:30:43 -0700

    Update Nav

--- src/components/StoreNavi/index.jsx
+++ src/components/StoreNavi/index.jsx
@@ -11,38 +11,52 @@ class StoreNavi extends Component {
     console.log("showStoreNav: " + showStoreNav)
     console.log("showProduct: " + showProduct)
 
-    projects.forEach((data) => {
-      const path = get(data, 'project.id')
-      const type = get (data, 'project.productType')
+    if(showStoreNav == false) {
+      catLinks.push(
+        <div className="nav justify-content-center text-uppercase" style={{display: "inherit"}} >
+        <li className="col-md-3 col-11 mt-6 mx-3" style={{}} >
+          <a className="nav-link" style={{cursor: 'pointer', padding: 0, boxShadow: '0px 0px 6px 1px rgba(0,0,0,0.2)'}} onClick={() => (
+                                                                        this.props.showHideProjects("Lenses"),
+                                                                        this.props.toggleStoreNav()
+                                                                      )} >
+            <img src={withPrefix(`/img/Lenses.jpg`)} style={{margin: 0, padding: 0}} />
+          </a>
+        </li>
+        <li className="col-md-3 col-11 mt-6 mx-3" style={{}} >
+          <a className="nav-link" style={{cursor: 'pointer', padding: 0, boxShadow: '0px 0px 6px 1px rgba(0,0,0,0.2)'}} onClick={() => (
+                                                                        this.props.showHideProjects("Tear-Offs"),
+                                                                        this.props.toggleStoreNav()
+                                                                      )} >
+            <img src={withPrefix(`/img/Tear-Offs.jpg`)} style={{margin: 0, padding: 0}} />
+          </a>
+        </li>
+        <li className="col-md-3 col-11 mt-6 mx-3" style={{}} >
+          <a className="nav-link" style={{cursor: 'pointer', padding: 0, boxShadow: '0px 0px 6px 1px rgba(0,0,0,0.2)'}} onClick={() => (
+                                                                        this.props.showHideProjects("Accesories"),
+                                                                        this.props.toggleStoreNav()
+                                                                      )} >
+            <img src={withPrefix(`/img/Accesories.jpg`)} style={{margin: 0, padding: 0}} />
+          </a>
+        </li>
+        </div>
+      )
+    }
+    else {
+      catLinks.push(
+        <div style={{display: "inherit"}} >
+        <li >
+          <a className="nav-link" style={{cursor: 'pointer'}} onClick={() => this.props.showHideProjects("Lenses")} >Lenses</a>
+        </li>
+        <li >
+          <a className="nav-link" style={{cursor: 'pointer'}} onClick={() => this.props.showHideProjects("Tear-Offs")} >Tear-Offs</a>
+        </li>
+        <li >
+          <a className="nav-link" style={{cursor: 'pointer'}} onClick={() => this.props.showHideProjects("Accesories")} >Tear-Accesories</a>
+        </li>
+        </div>
+      )
+    }
 
-      console.log("type: " + type)
-      console.log("path: " + path)
-
-      if (type != null) {
-          if (catList.indexOf(type) == -1) {
-            catList.push(type)
-            if(showStoreNav == false) {
-              catLinks.push(
-                <li key={path} className="col-md-3 col-11 mt-6 mx-3" style={{}} >
-                  <a className="nav-link" style={{cursor: 'pointer', padding: 0, boxShadow: '0px 0px 6px 1px rgba(0,0,0,0.2)'}} onClick={() => (
-                                                                                this.props.showHideProjects(type),
-                                                                                this.props.toggleStoreNav()
-                                                                              )} >
-                    <img src={withPrefix(`/img/${type}.jpg`)} style={{margin: 0, padding: 0}} />
-                  </a>
-                </li>
-              )
-            }
-            else {
-              catLinks.push(
-                <li key={path} >
-                  <a className="nav-link" style={{cursor: 'pointer'}} onClick={() => this.props.showHideProjects(type)} >{type}</a>
-                </li>
-              )
-            }
-          }
-      }
-    })
     return (
       <div id="store-nav" className="row justify-content-center">
         <div className="col-md-8 col-11">
